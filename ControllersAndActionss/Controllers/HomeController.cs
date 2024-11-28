edf6f4083b73b18d8f7922914f95e4a8323d3797
Abhishek <Abhishekgwari14sep@gmail.com>
2024-11-28 22:32:18 +0530

    View Bag and View Data

--- ControllersAndActionss/Controllers/HomeController.cs
+++ ControllersAndActionss/Controllers/HomeController.cs
@@ -6,6 +6,38 @@ namespace ControllersAndActionss.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Data1 = "This is Abhishek gwari";
+            ViewBag.Data2 = 47;
+            ViewBag.Data3 = DateTime.Now.ToShortDateString();
+
+            ViewData["data6"] = "This is view data but can access through view bag ";
+            ViewBag.data7 = "This is view bag but can access through view data ";
+
+            string[] arr = { "Abhishek ", "Aman", "Lokesh" };
+            ViewBag.Data4 = arr;
+
+            ViewBag.Data5 = new List<string>()
+            {
+            "Cricket ", "Football", "Hockey"
+
+                };
+
+
+
+
+
+
+                //ViewData ["Data1"] = "Abhishek gwari";
+                //ViewData["Data2"] = 2;
+                //ViewData["Data3"] = DateTime.Now.ToLongDateString();
+
+                //string[] arr = { "Abhishek ", "Aman", "Lokesh" };
+                //ViewData["Data4"] = arr;
+
+                //ViewData["Data5"] = new List<string>() {             
+                //"Cricket ", "Football", "Hockey"
+
+                //};
             return View();
         }
         public IActionResult About()
