bfd9254213e155e0ff5dce90012aae9d936b38aa
Abhishek <Abhishekgwari14sep@gmail.com>
2024-11-29 20:39:46 +0530

    TempData

--- ControllersAndActionss/Controllers/HomeController.cs
+++ ControllersAndActionss/Controllers/HomeController.cs
@@ -6,42 +6,23 @@ namespace ControllersAndActionss.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Data1 = "This is Abhishek gwari";
-            ViewBag.Data2 = 47;
-            ViewBag.Data3 = DateTime.Now.ToShortDateString();
+            ViewData["Data1"] = "Abhishek gwari view DATA";
+            ViewBag.Data2 = "Abhishek gwari view bag";
+            TempData["Data3"] = "Abhishek gwari Temp DATA";
+            TempData.Keep("Data3");                          // THIS WILL USE IN EVERY PAGE
 
-            ViewData["data6"] = "This is view data but can access through view bag ";
-            ViewBag.data7 = "This is view bag but can access through view data ";
-
-            string[] arr = { "Abhishek ", "Aman", "Lokesh" };
-            ViewBag.Data4 = arr;
-
-            ViewBag.Data5 = new List<string>()
-            {
-            "Cricket ", "Football", "Hockey"
-
-                };
+            // TempData["Data4"] = new List<string>()
+            // {
+            //"Cricket ", "Football", "Hockey" 
+            // };
 
 
-
-
-
-
-                //ViewData ["Data1"] = "Abhishek gwari";
-                //ViewData["Data2"] = 2;
-                //ViewData["Data3"] = DateTime.Now.ToLongDateString();
-
-                //string[] arr = { "Abhishek ", "Aman", "Lokesh" };
-                //ViewData["Data4"] = arr;
-
-                //ViewData["Data5"] = new List<string>() {             
-                //"Cricket ", "Football", "Hockey"
-
-                //};
             return View();
         }
         public IActionResult About()
         {
+
+            TempData.Keep("Data3");
             return View();
         }
         public IActionResult Contact()
@@ -59,3 +40,34 @@ namespace ControllersAndActionss.Controllers
         }
     }
 }
+
+
+//ViewBag.Data1 = "This is Abhishek gwari";
+//ViewBag.Data2 = 47;
+//ViewBag.Data3 = DateTime.Now.ToShortDateString();
+
+//ViewData["data6"] = "This is view data but can access through view bag ";
+//ViewBag.data7 = "This is view bag but can access through view data ";
+
+//string[] arr = { "Abhishek ", "Aman", "Lokesh" };
+//ViewBag.Data4 = arr;
+
+//ViewBag.Data5 = new List<string>()
+//{
+//"Cricket ", "Football", "Hockey"
+
+//    };
+
+
+
+//ViewData ["Data1"] = "Abhishek gwari";
+//ViewData["Data2"] = 2;
+//ViewData["Data3"] = DateTime.Now.ToLongDateString();
+
+//string[] arr = { "Abhishek ", "Aman", "Lokesh" };
+//ViewData["Data4"] = arr;
+
+//ViewData["Data5"] = new List<string>() {             
+//"Cricket ", "Football", "Hockey"
+
+//};
